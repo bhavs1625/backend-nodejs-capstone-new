3ecbcb7d4404a22dc037ace229a6ebdeb2f30465
bhavs1625 <bhavs1625@gmail.com>
2024-12-23 16:16:21 +0530

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
-
-        
-        const collection = db.collection("gifts");
-
+        const db = await connectToDatabase();
+        const collection = db.collection(process.env.MONGO_COLLECTION);
         // Initialize the query object
         let query = {};
 
@@ -19,27 +16,18 @@ router.get('/', async (req, res, next) => {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
 
-        // Task 3: Add other filters to the query
+        // Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
-            // query.condition = req.query.condition;
-            // query.age_years = { $lte: parseInt(req.query.age_years) };
         }
         if (req.query.condition) {
-            // {{insert code here}} 
-            // query.category = req.query.category;
             query.condition = req.query.condition;
-            // query.age_years = { $lte: parseInt(req.query.age_years) };
         }
         if (req.query.age_years) {
-            // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-        const items=await collection.find(query).toArray();
-
+        const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
         next(e);
