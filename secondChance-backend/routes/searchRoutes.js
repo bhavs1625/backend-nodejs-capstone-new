508615528c35a877be6a970749420fff91089cf4
bhavs1625 <bhavs1625@gmail.com>
2024-12-23 16:13:15 +0530

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -8,22 +8,28 @@ router.get('/', async (req, res, next) => {
         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         // {{insert code here}}
 
+        
         const collection = db.collection("gifts");
 
         // Initialize the query object
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+        }
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
-            // {{insert code here}}
+            query.category = req.query.category;
+            // query.condition = req.query.condition;
+            // query.age_years = { $lte: parseInt(req.query.age_years) };
         }
         if (req.query.condition) {
             // {{insert code here}} 
+            // query.category = req.query.category;
+            query.condition = req.query.condition;
+            // query.age_years = { $lte: parseInt(req.query.age_years) };
         }
         if (req.query.age_years) {
             // {{insert code here}}
@@ -32,6 +38,7 @@ router.get('/', async (req, res, next) => {
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
         // {{insert code here here}}
+        const items=await collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
